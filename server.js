dd51501ed6562fa988cad0ad8a81a9d7a9c1c24c
Ryan Park <gotslaqks12@gmail.com>
2022-06-01 12:48:58 -0400

    mvp required just css

--- server.js
+++ server.js
@@ -21,88 +21,104 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 // Body parser middleware: give us access to req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static('public'));
 
 
 app.get('/ryancf', (req, res) => {
-    coffee.find({}, (error, allCoffee) => {
-        res.render("index.ejs", {
-          coffee: allCoffee,
-        });
-      });
+  coffee.find({}, (error, allCoffee) => {
+    res.render("index.ejs", {
+      coffee: allCoffee,
+    });
+  });
 });
 
 // routes
 // I
 
 app.get('/ryancf/coffee', (req, res) => {
-    coffee.deleteMany({}, (error, allCoffee) => { });
-    coffee.create(coffeeInfo, (error, data) => {
-      res.redirect("/ryancf");
-    });
+  coffee.deleteMany({}, (error, allCoffee) => { });
+  coffee.create(coffeeInfo, (error, data) => {
+    res.redirect("/ryancf");
+  });
 });
 
 // N
 
-app.get("/coffee/new", (req, res) => {
-    res.render("new.ejs");
-  });
+app.get("/ryancf/new", (req, res) => {
+  res.render("new.ejs");
+});
 
 // D
 
 app.delete("/ryancf/:id", (req, res) => {
-    coffee.findByIdAndDelete(req.params.id, (error, deletedcoffee) => {
-      res.redirect('/ryancf')
-    });
+  coffee.findByIdAndDelete(req.params.id, (error, deletedcoffee) => {
+    res.redirect('/ryancf')
   });
+});
 
 // U
 
 app.put("/ryancf/:id", (req, res) => {
-    coffee.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true },
-      (error, updatecoffee) => {
-        res.redirect('/ryancf');
-      }
-    );
-  });
+  coffee.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (error, updatecoffee) => {
+      res.redirect('/ryancf');
+    }
+  );
+});
 
 // C
 
 app.post("/ryancf", (req, res) => {
-    coffee.create(req.body, (error, createdcoffee) => {
-      res.redirect("/ryancf");
-    });
+  coffee.create(req.body, (error, createdcoffee) => {
+    res.redirect("/ryancf");
   });
+});
 
 // E
 
 app.get("/ryancf/:id/edit", (req, res) => {
-    coffee.findById(req.params.id, (error, foundcoffee) => {
-      res.render("edit.ejs", {
-        coffee: foundcoffee,
-      });
+  coffee.findById(req.params.id, (error, foundcoffee) => {
+    res.render("edit.ejs", {
+      coffee: foundcoffee,
     });
   });
+});
 
 // show
 
 app.get('/ryancf/:id', (req, res) => {
-    coffee.findById(req.params.id, (err, foundcoffee) => {
-      res.render('show.ejs', {
-        coffee: foundcoffee
-      });
+  coffee.findById(req.params.id, (err, foundcoffee) => {
+    res.render('show.ejs', {
+      coffee: foundcoffee
     });
   });
+});
 
 
-app.put("/products/:id/buy"), (req, res) => {
-    Product.Save(
-      req.body.id,
-      req.body,
-    )
-  };
+app.put("/ryancf/:id/buy"), (req, res) => {
+  coffee.Save(
+    req.body.id,
+    req.body,
+    { new: true },
+    (error, updatecoffee) => {
+      res.redirect('/ryancf');
+    }
+  );
+};
+
+app.post('/ryancf/:id/buy', (req, res) => {
+  coffee.findById(req.params.id, (err, data) => {
+    if (data.qty <= 0) {
+      data.qty = ' out of stock '
+    } else {
+      data.qty--;
+      data.save();
+    }
+  })
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
